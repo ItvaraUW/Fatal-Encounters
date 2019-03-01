a25d8fde91bc6791f629eeadd50187f6059faa6f
Bernabe Ibarra <bibarra@uw.edu>
2019-03-01 11:56:43 -0800

    establishing structure
    I need to figure out what the best practice in organizing a project is

---
+++ data/data.R
@@ -0,0 +1,8 @@
+# title: Final Project: Fatal Encounters
+# subtitle: "Data Script"
+# author: "Bernabe Ibarra | Feng Yu Yeh | Sean Le | Thomas That"
+# date: 20190301
+
+# library("googlesheets") // to maybe use later as api access
+
+fatal_encounters_df <- read.csv("Fatal_Encouters_dataset.csv", stringsAsFactors = F)
