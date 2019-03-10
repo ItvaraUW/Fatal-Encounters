604b214a81e6a62d732ad509053d6cda069014c7
Thomas <thomas.tat@hotmail.com>
2019-03-10 14:58:58 -0700

    Made line graphs for death growth rates

--- scripts/death_growth_rate.R
+++ scripts/death_growth_rate.R
@@ -1,1 +1,27 @@
-library()
+library(dplyr)
+library(ggplot2)
+
+# Creates a list of labels for whichever column name is chosen
+# for the function
+key <- list(
+  Subject.s.gender = "Gender",
+  Cause.of.death = "Cause of Death"
+)
+
+# Creates a line graph of the categories given by the user
+# and is plotted by years and number of deaths
+death_rate_growth <- function(data_frame, ..., col_name) {
+  data_frame %>%
+    filter(get(col_name) %in% c(...)) %>%
+    group_by_(col_name, "Date..Year.") %>%
+    count() %>%
+    ggplot(aes(x = Date..Year., y = n, color = get(col_name))) +
+    geom_line() +
+    geom_point() +
+    labs(
+      title = "Death Growth Rate",
+      x = "Year",
+      y = "Number of Deaths",
+      color = key[col_name]
+    )
+}
