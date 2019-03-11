326bb29bbb9328515ee4c8dbe6b6ae6b37e1594b
Sean Le <itvara@uw.edu>
2019-03-11 15:30:28 -0700

    Changed Selection of state from text to selected

--- ui.R
+++ ui.R
@@ -41,6 +41,60 @@ races <- list(
   "Race unspecified" = "Race unspecified"
 )
 
+states <- list(
+  "United States" = "All",
+  "Alabama" = "AL",
+  "Alaska" = "AK",
+  "Arizona" = "AZ",
+  "Arkansas" = "AR",
+  "California" = "CA",
+  "Colorado" = "CO",
+  "Connecticut" = "CT",
+  "Delaware" = "DE",
+  "Florida" = "FL",
+  "Georgia" = "GA",
+  "Hawaii" = "HI",
+  "Idaho" = "ID",
+  "Illinois" = "IL",
+  "Indiana" = "IN",
+  "Iowa" = "IA",
+  "Kansas" = "KS",
+  "Kentucky" = "KY",
+  "Louisiana" = "LA",
+  "Maine" = "ME",
+  "Maryland" = "MD",
+  "Massachusetts" = "MA",
+  "Michigan" = "MI",
+  "Minnesota" = "MN",
+  "Mississippi" = "MS",
+  "Missouri" = "MO",
+  "Montana" = "MT",
+  "Nebraska" = "NE",
+  "Nevada" = "NV",
+  "New Hampshire" = "NH",
+  "New Jersey" = "NJ",
+  "New Mexico" = "NM",
+  "New York" = "NY",
+  "North Carolina" = "NC",
+  "North Dakota" = "ND",
+  "Ohio" = "OH",
+  "Oklahoma" = "OK",
+  "Oregon" = "OR",
+  "Pennsylvania" = "PA",
+  "Rhode Island" = "RI",
+  "South Carolina" = "SC",
+  "South Dakota" = "SD",
+  "Tennessee" = "TN",
+  "Texas" = "TX",
+  "Utah" = "UT",
+  "Vermont" = "VT",
+  "Virginia" = "VA",
+  "Washington" = "WA",
+  "West Virginia" = "WV",
+  "Wisconsin" = "WI",
+  "Wyoming" = "WY"
+)
+
 factors <- list(
   "Race" = "race",
   "Gender" = "gender",
@@ -122,10 +176,11 @@ shinyUI(
       title = "Distrubution by State",
       sidebarLayout(
         sidebarPanel(
-          textInput(
+          selectInput(
             inputId = "state_in",
             label = "State",
-            value = "WA"
+            choices = states,
+            selected = "WA"
           ),
           radioButtons(
             inputId = "gender_in",
