26ce6d193f1cd56c9c7f3d06fb21d920c9d3444a
Feng Yu Yeh <fengy17@uw.edu>
2019-03-10 20:15:57 -0700

    add variable

--- scripts/donut_chart.R
+++ scripts/donut_chart.R
@@ -1,13 +1,13 @@
 library(dplyr)
 library(plotly)
-
+source(data/data.R)
 
 # Make an interactive donut chart that shows individuals within each group 
 # that was killed to fatal encounters.
 
 donut_chart <- function(data_frame_num, variable){
   data_for_graph <- data_frame_num %>%
-    select(Unique.ID, Subject.s.gender, Subject.s.race, 
+    select(Subject.s.gender, Subject.s.race, 
            Cause.of.death, Date..Year.) %>%
     rename(subject_gender = Subject.s.gender,
            subject_race = Subject.s.race,
@@ -15,19 +15,19 @@ donut_chart <- function(data_frame_num, variable){
            year = Date..Year.)
 
   interactive_plot <- data_for_graph %>% 
-    group_by(year) %>% 
-    summarise(freq = length(year)) %>% 
+    group_by(variable) %>% 
+    summarise(freq = length(variable)) %>% 
     plot_ly(
-      labels = ~year,
+      labels = ~variable,
       values = ~freq,
       type = "pie",
       textinfo = 'percent',
       hoverinfo = 'text',
-      text = ~paste(freq, year),
+      text = ~paste(freq, variable),
       hole = 0.6
     ) %>% 
     layout(
-      title = paste("Total Police Fatal Encounter by"),
+      title = paste("Total Police Fatal Encounter by", variable),
       xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
       yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE)
     )
