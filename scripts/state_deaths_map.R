fb98b8e5613d7b3e684c65c42c9ccbcf32b12c3d
Bernabe Ibarra <bibarra@uw.edu>
2019-03-10 21:55:15 -0700

    Reorganized script as file
    Data will be handled by data script

--- scripts/state_deaths_map.R
+++ scripts/state_deaths_map.R
@@ -9,42 +9,34 @@
 # States interactive map
 ################################################################################
 
-# source("../data/data.R")
-
-FE_df <- read.csv("../data/Fatal_Encounters_data.csv", stringsAsFactors = F)
-
 library(tidyverse) # includes: ggplot2 dplyr tidyr stringr
 library(leaflet)
 
-profile <- FE_df[!(grepl(
-  "2100", FE_df$Date..Year.
-)), ] %>%
-  rename(
-    name = Subject.s.name,
-    age = Subject.s.age,
-    gender = Subject.s.gender,
-    race = Subject.s.race,
-    state = Location.of.death..state.,
-    date = Date.of.injury.resulting.in.death..month.day.year.,
-    year = Date..Year.
-  ) %>%
-  select(name, age, gender, race, state, date, Longitude, Latitude, year) %>%
-  filter(state %in% state.abb) %>%
-  filter(state == input$state_in) %>%
-  filter(age == input$age_in & gender == input$gender_in &
-    race == input$race_in | year == input$year_in)
-# filter(state == "NY") %>%
-# filter(age == 24 & gender == "Female" &
-#      race == "Race unspecified" | year == 2000)
-
-points_map <- profile %>%
-  leaflet(options = leafletOptions(
-    dragging = F,
-    minZoom = 5,
-    maxZoom = 10
-  )) %>%
-  addProviderTiles("CartoDB.Positron") %>%
-  addCircleMarkers(
-    lat = ~Latitude, lng = ~Longitude,
-    label = ~paste(name, date, sep = " - "), radius = 1
-  )
+make_map <- function(FE_df) {
+  profile <- FE_df %>%
+    rename(
+      name = Subject.s.name,
+      age = Subject.s.age,
+      gender = Subject.s.gender,
+      race = Subject.s.race,
+      state = Location.of.death..state.,
+      date = Date.of.injury.resulting.in.death..month.day.year.,
+      year = Date..Year.
+    ) %>%
+    select(name, age, gender, race, state, date, Longitude, Latitude, year) %>%
+    filter(state %in% state.abb) %>%
+    filter(state == input$state_in) %>%
+    filter(age == input$age_in & gender == input$gender_in &
+      race == input$race_in | year == input$year_in)
+  points_map <- profile %>%
+    leaflet(options = leafletOptions(
+      dragging = F,
+      minZoom = 5,
+      maxZoom = 10
+    )) %>%
+    addProviderTiles("CartoDB.Positron") %>%
+    addCircleMarkers(
+      lat = ~Latitude, lng = ~Longitude,
+      label = ~paste(name, date, sep = " - "), radius = 2
+    )
+}
