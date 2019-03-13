32a000f8cfd87d1cce208a38ec6ba7807cddef91
Feng Yu Yeh <fengy17@uw.edu>
2019-03-13 11:31:55 -0700

    add comments

--- scripts/donut_chart.R
+++ scripts/donut_chart.R
@@ -5,6 +5,9 @@ library(plotly)
 # that was killed to fatal encounters.
 
 donut_chart <- function(data_frame, sym_factor, factor){
+  # acquire data from input data frame
+  # change some error from the data frame 
+  # and select needed variables only
   data_for_graph <- data_frame %>%
     mutate(
       Subject.s.gender = replace(Subject.s.gender,
@@ -18,23 +21,25 @@ donut_chart <- function(data_frame, sym_factor, factor){
            cause = Cause.of.death,
            year = Date..Year.)
 
+  # plot data
   interactive_plot <- data_for_graph %>% 
     group_by(!!sym_factor) %>% 
-    summarise(freq = length(!!sym_factor)) %>% 
-    filter(!(is.na(!!sym_factor))) %>% 
+    summarise(freq = length(!!sym_factor)) %>% # calculate the frequency happen
+    filter(!(is.na(!!sym_factor))) %>% # filter out if there's any NAs
     plot_ly(
-      labels = ~eval(parse(text = factor)),
-      values = ~freq,
-      type = "pie",
-      textinfo = 'percent',
-      hoverinfo = 'text',
+      labels = ~eval(parse(text = factor)), #
+      values = ~freq, # set values from the frequency calculated
+      type = "pie", # create pie chart
+      textinfo = 'percent', # show percentage on the plot
+      hoverinfo = 'text', # determine which information to appear when hover
       text = ~paste("People Killed: ", freq, "<br>", 
                     toupper(substr(sym_factor, 1, 1)), 
                     substring(sym_factor, 2), sep = "", ": ",
-                    eval(parse(text = factor))),
-      hole = 0.6
+                    eval(parse(text = factor))), # show the information 
+                                                 # when hovering over a bar
+      hole = 0.6 # make a hole at the pie chart to make a donut shape
     ) %>% 
-    layout(
+    layout( # set up the title of the plot and get rid or x and y axis
       title = paste("Total Police Fatal Encounter by ",
                     toupper(substr(factor, 1, 1)), 
                     substring(factor, 2), sep = ""),
