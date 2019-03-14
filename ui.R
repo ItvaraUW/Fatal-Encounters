cc97a395bf07230a43bc5e2f49347f74f2538e5a
Thomas <thomas.tat@hotmail.com>
2019-03-14 03:36:41 -0700

    made new css to change theme and added data analysis tab

--- ui.R
+++ ui.R
@@ -4,7 +4,6 @@
 # date: "20190301"
 
 library(shiny)
-library(shinythemes)
 library(plotly)
 library(leaflet)
 
@@ -107,7 +106,7 @@ factors <- list(
 shinyUI(
   navbarPage(
     title = "Fatal Encounters",
-    theme = shinytheme("yeti"),
+    theme = "style.css",
     
     # Project Description Tab
     tabPanel(
@@ -252,6 +251,52 @@ shinyUI(
         mainPanel(
           plotlyOutput("donut_chart")
         )
+      )
+    ),
+    
+    # Data Analysis tab
+    tabPanel(
+      title = "Data Analysis",
+      mainPanel(
+        h1("State Distribution Data"),
+        p("From the data given in the map, California has the highest amount of
+          deaths out of all the states and has the highest amount of deaths of
+          each race except Native American/Alaskan since 2002. Not only that,
+          but it is also the state that had the most deaths each year. This is
+          likely due to the size of California, but it is not the only factor
+          since big states like Texas didn't have as many deaths. There are a
+          few outliers on the map, but they are so little that they have no
+          effect on the patterns of the data."),
+        h1("Growth Rate Data"),
+        p("The Death Growth Rate data shows that highest number of victims to
+          these fatal encounters are males and the most common cause of deaths
+          was by gunshot. These two factors have seen steady growth throughout
+          the years along with many other people and causes. While the graphs
+          see a steep drop towards the end, that is due to the fact that the
+          full year of 2019 has not passed yet. 2019 could continue the trend
+          of growth by the end of the year"),
+        h1("Factors of Impact Data"),
+        p("This donut chart shows that there were more victims that were of an
+          unspecified race than many of the other races. It also showed us that
+          a majority of the victims were male. Many of the deaths were caused
+          by gunshot and the year that had the most deaths was 2018. Male
+          victims and death by gunshot seem to be a great majority in their
+          respective groups. However, victims of an unspecified race seem to
+          have a similar ratio to those who are White and those who are Black.
+          The years also seem to have a similar ratio of deaths. Although, it
+          should be known that other factors can affect the ratios of these
+          categories"),
+        h1("Use of the Data"),
+        p("From all the data visualizations, there have been many leading
+          factors of different categories. This could be used to identify the
+          problem and how it can be fixed. However, these factors should **not**
+          be treated as the main cause to all deaths by police officers. Many of
+          these factors affect the other's outcomes. Our purpose was to provide
+          this information so that the public can know about this and make
+          informed decisions based on it. Instead of focusing on one category,
+          the whole set of data should be used to identify the problem. Our hope
+          is that people become more informed and more understanding of the
+          problem this way.")
       )
     )
   )
